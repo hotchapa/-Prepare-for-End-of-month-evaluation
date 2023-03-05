2ea454b371e99f5b11e28c4d1bb47bc0c516eae6
hotchapa <hotchapa@naver.com>
2023-03-05 12:16:43 +0900

    PROBLEM UPDATE

---
+++ 문제 모음/추천 문제/[SWEA] 패턴 마디의 길이.py
@@ -0,0 +1,12 @@
+T = int(input())
+for tc in range(1,T+1):
+    str_list = input()
+    word = ""
+    for i in str_list:
+        word += i
+        length = len(word)
+        if word == str_list[length:length+length]:
+            break
+
+    print(f"#{tc} {len(word)}")
+
